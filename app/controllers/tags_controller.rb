ac84b6848bd8e67e17d3181373157952420000bc
Michael Coon <mac718@gmail.com>
2016-09-04 09:55:20 -0400

    Updated controllers

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,6 @@
 class TagsController < ApplicationController
 	before_filter :require_login, only: [:destroy]
 
-	def require_login
-    	unless current_user
-    	  redirect_to root_path
-     	 return false
-    	end
-  	end
-
 	def index
 		@tags = Tag.all
 	end
